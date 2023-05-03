727e2305b7fdabc161467117a8693c6561000fd2
mathewevine <mathewevine@gmail.com>
2023-05-03 08:47:08 +0000

    second Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const element = (
   //   Write your code here.
   <div className="container">
     <h1 className="heading">Super Over League</h1>
-    <div>
+    <div className="img-container">
       <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" />
       <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" />
     </div>
